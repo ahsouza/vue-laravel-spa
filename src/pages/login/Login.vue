3bd5e3e4ea969a7dfe87cd1f1046dd381b7bbcc2
Aníbal Henrique <annibalhsouza@gmail.com>
2019-03-21 23:29:02 -0300

    #ISSUE Implemeting form of login and register

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -6,40 +6,46 @@
     </span>
 
     <span slot="main">
-      <h2>Login </h2>
-      <input type="text" placeholder="E-mail" value="">
-      <input type="password" placeholder="Senha" value="">
-      <button type="button" class="btn">ENTRAR</button>
+
+      <span v-if="!register">
+        <h2>Login </h2>
+        <input type="text" placeholder="E-mail" value="">
+        <input type="password" placeholder="Senha" value="">
+        <button class="btn">ENTRAR</button>
+        <button class="btn orange" v-on:click="register = !register">CADASTRE-SE</button>
+      </span>
+
+      <span v-if="register">
+        <h2>Create Account </h2>
+        <input type="text" placeholder="Nome" value="">
+        <input type="text" placeholder="E-mail" value="">
+        <input type="password" placeholder="Senha" value="">
+        <input type="password" placeholder="Confirme sua senha" value="">
+        <button class="btn">ENVIAR</button>
+        <button class="btn orange" v-on:click="register = !register">Já possuo uma conta</button>
+      </span>
 
     </span>
+
   </LoginTemplate>
-
-
 </template>
 
 <script>
 import LoginTemplate from '@/templates/LoginTemplate'
-import CardContentVue from '@/components/patterns/CardContentVue'
-import CardDetailVue from '@/components/patterns/CardDetailVue'
-import PublicContentVue from '@/components/patterns/PublicContentVue'
 
 export default {
   name: 'Home',
   data () {
     return {
-
+      register: true
     }
   },
   components:{
-    CardContentVue,
-    CardDetailVue,
-    LoginTemplate,
-    PublicContentVue
+    LoginTemplate
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
